e199c7fe4bd9a4fff0eaf3a08162b6131e89b91e
xawwnee <76479202+xawwnee@users.noreply.github.com>
2023-02-01 21:45:10 +0530

    Update setHeader.js

--- helper/setHeader.js
+++ helper/setHeader.js
@@ -1,7 +1,7 @@
 module.exports = function (res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
-  res.setHeader("Open-Source", "https://github.com/xawwnee/JiosaavnAPI");
-  res.setHeader("Made-By", "xawwnee, https://github.com/xawwnee");
+  res.setHeader("Source", "https://radiobollyfm.in");
+  res.setHeader("Made-For", "radiobollyfm, https://radiobollyfm.in");
   return;
 };
