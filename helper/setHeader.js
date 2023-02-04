8ca4b344edc1c0ed444a33277670665f20227d88
xawwnee <76479202+xawwnee@users.noreply.github.com>
2023-02-04 15:26:35 +0530

    Update setHeader.js

--- helper/setHeader.js
+++ helper/setHeader.js
@@ -1,6 +1,5 @@
 module.exports = function (res) {
-  /*res.setHeader("Access-Control-Allow-Origin", "https://*.radiobollyfm.in");*/
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
   res.setHeader("server", "https://radiobollyfm.in");
   res.setHeader("Made-For", "radiobollyfm, https://radiobollyfm.in");
