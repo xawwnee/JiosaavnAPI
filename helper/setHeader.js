a264004274c4420dd0bbf0b3724320c4dbe2e79b
xawwnee <76479202+xawwnee@users.noreply.github.com>
2023-02-04 15:11:51 +0530

    Update CORS

--- helper/setHeader.js
+++ helper/setHeader.js
@@ -1,10 +1,6 @@
 module.exports = function (res) {
-  //res.setHeader("Access-Control-Allow-Origin", "https://*.radiobollyfm.in");
-  var allowedDomains = ['https://*.radiobollyfm.in','http://localhost:3000' ];
-  var origin = req.headers.origin;
-  if(allowedDomains.indexOf(origin) > -1){
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  /*res.setHeader("Access-Control-Allow-Origin", "https://*.radiobollyfm.in");*/
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
   res.setHeader("server", "https://radiobollyfm.in");
   res.setHeader("Made-For", "radiobollyfm, https://radiobollyfm.in");
