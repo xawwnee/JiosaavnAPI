56a36e908b5b454fed2f114f0c1c0b7b02411d38
xawwnee <76479202+xawwnee@users.noreply.github.com>
2024-02-07 13:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require("express")();
 const fs = require("fs");
 const swaggerUi = require("swagger-ui-express");
+var options = {
+  customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.1/themes/3.x/theme-outline.min.css'
+};
 
 // initially i build it to run it in top of vercel, so i had to do this hacky way
 fs.readdirSync(`${__dirname}/api`).forEach((file) => {
@@ -9,7 +12,7 @@ fs.readdirSync(`${__dirname}/api`).forEach((file) => {
   app.use(`/${routename}`, require(`${__dirname}/api/${file}`));
 });
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./docs/swagger"))); // swagger-ui-express
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./docs/swagger"), options)); // swagger-ui-express
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
