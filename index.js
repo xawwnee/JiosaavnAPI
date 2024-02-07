6cd6fbcf5423236e6bb908f00f1279ab054ee834
xawwnee <76479202+xawwnee@users.noreply.github.com>
2024-02-07 13:33:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 const fs = require("fs");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('./docs/swagger');
+
 var options = {
   customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.1/themes/3.x/theme-outline.min.css'
 };
@@ -12,7 +14,7 @@ fs.readdirSync(`${__dirname}/api`).forEach((file) => {
   app.use(`/${routename}`, require(`${__dirname}/api/${file}`));
 });
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./docs/swagger"), options)); // swagger-ui-express
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options)); // swagger-ui-express
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
